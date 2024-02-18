b9d8b26edf87c7317e9107f617bc2b915067d793
Akangkha Sarkar <akangkhasarkar@gmail.com>
2024-02-18 16:34:24 +0530

    Constructed Responsive Navbar component Tailoured the tailwind.config.js file to include custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -18,15 +18,25 @@ module.exports = {
     },
     extend: {
       colors: {
+        "color-1": "#E1C3FF",
+        "color-2": "#9D5AE3",
+        "color-3": "#663B9C",
+        "color-4": "#3C3D8E",
+        "color-5": "#3D306F",
+        "color-6": "#161A33",
+        primary: "#0F1629",
+        fontFamily: {
+          sans: ["Graphik", "sans-serif"],
+          serif: ["Merriweather", "serif"],
+          poppins: ["Poppins", "sans-serif"],
+          the_bold_font: ["The Bold Font"],
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
+
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
@@ -71,7 +81,16 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-horizon":
+          "linear-gradient(90deg, #FFF 2.09%, #9D5AE3 75.49%)",
+        "gradient-hero-img":
+          "radial-gradient(50% 50% at 50% 50%, rgba(128, 116, 255, 0.67) 0%, rgba(123, 112, 238, 0.00) 100%)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
