ceb4261a4c2ce67332dfff3ef0b19cfc4553d8a4
Shruti Priya <priyashruti337@gmail.com>
2024-02-18 20:34:09 +0530

    created feature component

---
+++ src/components/features/page.js
@@ -0,0 +1,69 @@
+import Image from 'next/image'
+import learning from "../images/learning.png";
+
+const Features = () => {
+  return (
+    <section>
+      <div className="flex justify-center">
+        <p className="text-3xl font-sans font-bold bg-gradient-to-r from-white to-purple-500 inline-block text-transparent bg-clip-text">
+          FEATURES
+        </p>
+      </div>
+
+      <div className='pt-20 ml-50 mr-40'>
+        <div className='flex justify-evenly'>
+        <div>
+          <Image src={learning} height={500} width={500}/>
+        </div>
+        <div className='ml-40 w-748 h-135'>
+          <p className='text-3xl font-poppins leading-7 tracking-tighter font-bold uppercase bg-gradient-to-r from-white to-purple-500 bg-clip-text text-transparent inline-block'>
+            <p className='text-white'>Created learning path to</p> <p>make you learn what matters</p>
+          </p>
+          <p className='text-white font-poppins pt-5'>
+            Embrace wisdom's compass, learn what truly holds value. In the journey of knowledge, discern the essence, learn what matters.
+          </p>
+          <button class="bg-purple-500 text-white py-2 px-4 rounded-full mt-5 shadow-lg shadow-purple-500/40">
+            Explore Now
+          </button>
+        </div>
+        </div>
+
+        <div className='flex justify-center'>
+        <div className='ml-40'>
+          <p className='text-3xl font-poppins leading-7 tracking-tighter font-bold uppercase bg-gradient-to-r from-white to-purple-500 bg-clip-text text-transparent inline-block'>
+            <p className='text-white'>Build and interact with</p> <p>your community</p>
+          </p>
+          <p className='text-white font-poppins pt-5'>
+          Building community requires empathy, communication, and a shared vision. Together, we construct bridges of understanding and support, fostering a tapestry of belonging and collaboration.
+          </p>
+          <button class="bg-purple-500 text-white py-2 px-4 rounded-full mt-5 shadow-lg shadow-purple-500/40">
+            Build Now
+          </button>
+        </div>
+        <div>
+          <Image src={community.png} width={500} height={500} alt="Picture"/>
+        </div>
+        </div>
+
+        <div className='flex justify-evenly'>
+        <div>
+          <Image src="/public/images/learning.png" width={500} height={500} alt="Picture"/>
+        </div>
+        <div className='ml-40 w-748 h-135'>
+          <p className='text-3xl font-poppins leading-7 tracking-tighter font-bold uppercase bg-gradient-to-r from-white to-purple-500 bg-clip-text text-transparent inline-block'>
+            <p className='text-white'>track you progress</p><p>while you learn</p> 
+          </p>
+          <p className='text-white font-poppins pt-5'>
+            Measure your steps, chart your course, and let each milestone be a testament to your journey's force.
+          </p>
+          <button class="bg-purple-500 text-white py-2 px-4 rounded-full mt-5 shadow-lg shadow-purple-500/40">
+            Start Now
+          </button>
+        </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Features
