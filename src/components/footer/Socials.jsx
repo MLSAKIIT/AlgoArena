b5d8b27728def74f76abe545a30ae572f7eb92af
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-02-19 01:15:25 +0530

    separated gradient img

--- src/components/footer/Socials.jsx
+++ src/components/footer/Socials.jsx
@@ -3,13 +3,12 @@ import {FaGithubSquare} from "react-icons/fa";
 import {FaInstagram} from "react-icons/fa";
 import {FaFacebook} from "react-icons/fa";
 import "../../app/globals.css";
+import GradientImg from "./GradientImg";
 
 const socials = () => {
     return (
         <div className="relative flex flex-col bottom-0 sm:flex-row justify-between w-full items-center box-border mt-24 ">
-            <div className="img_div absolute bottom-0 left-0">
-                <img className="hidden sm:block w-3/6 md:w-3/6 lg:w-3/6 " src="ellipse.svg" alt="gradient " />
-            </div>
+            <GradientImg />
             <div className="mb-4 sm:mb-12 sm:pl-8 text-white sm:text-3xl text-2xl z-10 text-center ">AlgoArena x MLSA</div>
             <div className="flex text-[#9747ff] sm:mb-12 m-4">
                 <div className="icons mx-2  hover:text-white cursor-pointer active:scale-95">
