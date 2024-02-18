08e8dd8ebe8b3aa3c9c4510c4cc342c850cc80ff
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-02-19 00:24:53 +0530

    Fixed the zoom in zoom out issue from suggestions and footer

--- src/components/footer/Socials.jsx
+++ src/components/footer/Socials.jsx
@@ -6,26 +6,32 @@ import "../../app/globals.css";
 
 const socials = () => {
     return (
-        <div className="flex text-[#9747ff]">
-            <div className="icons mx-2  hover:text-white cursor-pointer active:scale-95">
-                <a href="#">
-                    <FaGithubSquare size={30} />
-                </a>
+        <div className="relative flex flex-col bottom-0 sm:flex-row justify-between w-full items-center box-border mt-24 ">
+            <div className="img_div absolute bottom-0 left-0">
+                <img className="hidden sm:block w-3/6 md:w-3/6 lg:w-3/6 " src="ellipse.svg" alt="gradient " />
             </div>
-            <div className="icons mx-2  hover:text-white cursor-pointer active:scale-95">
-                <a href="#">
-                    <IoLogoDiscord size={30}></IoLogoDiscord>
-                </a>
-            </div>
-            <div className="icons mx-2 hover:text-white cursor-pointer active:scale-90">
-                <a href="#">
-                    <FaInstagram size={30}></FaInstagram>
-                </a>
-            </div>
-            <div className="icons mx-2 hover:text-white cursor-pointer active:scale-95">
-                <a href="#">
-                    <FaFacebook size={30}></FaFacebook>
-                </a>
+            <div className="mb-4 sm:mb-12 sm:pl-8 text-white sm:text-3xl text-2xl z-10 text-center ">AlgoArena x MLSA</div>
+            <div className="flex text-[#9747ff] sm:mb-12 m-4">
+                <div className="icons mx-2  hover:text-white cursor-pointer active:scale-95">
+                    <a href="#">
+                        <FaGithubSquare size={30} />
+                    </a>
+                </div>
+                <div className="icons mx-2  hover:text-white cursor-pointer active:scale-95">
+                    <a href="#">
+                        <IoLogoDiscord size={30}></IoLogoDiscord>
+                    </a>
+                </div>
+                <div className="icons mx-2 hover:text-white cursor-pointer active:scale-90">
+                    <a href="#">
+                        <FaInstagram size={30}></FaInstagram>
+                    </a>
+                </div>
+                <div className="icons mx-2 hover:text-white cursor-pointer active:scale-95">
+                    <a href="#">
+                        <FaFacebook size={30}></FaFacebook>
+                    </a>
+                </div>
             </div>
         </div>
     );
