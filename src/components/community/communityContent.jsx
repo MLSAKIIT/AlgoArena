ffab35b6bd54bbf4ef94cee6853a1039307eb8f8
Shruti Priya <priyashruti337@gmail.com>
2024-03-07 21:42:54 +0530

    community page

--- src/components/community/communityContent.jsx
+++ src/components/community/communityContent.jsx
@@ -19,7 +19,7 @@ const Content = () => {
       )}
       {expanded && (
         <div>
-          <p>olor sit amet consectetur adipisicing elit. Doloremque illum numquam corrupti quasi maiores laboriosam neque repudiandae quibusdam, quam ipsam tempore itaque obcaecati et ratione debitis, unde iusto omnis id unde iusto omnis id omnis id
+          <p>olor sit amet consectetur  elit. Doloremque illum numquam corrupti quasi maiores laboriosam neque repudiandae quibusdam, quam ipsam tempore itaque obcaecati et ratione debitis, unde iusto omnis id unde iusto omnis id omnis id
             </p>  
             
         </div>
