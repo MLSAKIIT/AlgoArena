d9809260ba955cada2cdf3f3a4f2f45edee5a9a1
Rupoum <rupoummitra15278@gmail.com>
2024-03-09 03:18:34 +0530

    improved the forms made the email veri status

--- src/app/(auth)/new-password/page.jsx
+++ src/app/(auth)/new-password/page.jsx
@@ -8,8 +8,7 @@ const NewPasswordPage = ({ searchParams }) => {
   }
   return (
     <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
-      {/* <NewPasswordForm token={searchParams.token} /> */}
-      <NewPasswordForm />
+      <NewPasswordForm token={searchParams.token} />
     </div>
   );
 };
