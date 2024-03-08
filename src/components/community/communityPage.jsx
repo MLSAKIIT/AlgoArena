a46f0caa1e1a8ce14926b94caacb05c86323695c
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-09 02:51:09 +0530

    Functionality Made for Desktop

--- src/components/community/communityPage.jsx
+++ src/components/community/communityPage.jsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import CommunityPosts from "./communityPosts";
 import { PiFunnel } from "react-icons/pi";
@@ -7,139 +7,153 @@ import { CgProfile } from "react-icons/cg";
 import { VscTriangleUp } from "react-icons/vsc";
 import { RxCaretDown } from "react-icons/rx";
 import { RxCaretUp } from "react-icons/rx";
-import Link from 'next/link';
+import Link from "next/link";
+import { getPosts } from "@/actions/post";
+import { postFilter, searchFilter } from "@/lib/postFilter";
 
 const Community = () => {
+  const [condition, setCondition] = useState(null);
+  const [search, setSearch] = useState("");
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    getPosts()
+      .then((res) => setPosts(res))
+      .catch((err) => console.log(err));
+  }, []);
+  const buttonNames = ["Recent", "Popularity", "Upvotes", "Domain"];
+  const [isOpen, setIsOpen] = useState(false);
 
-    const buttonNames = ["Popularity", "Recent", "Upvotes", "Domain"];
-
-    const buttonElements = buttonNames.map((name, index) => (
-    <button
-        key={index}
-        className="w-22 h-10 md:w-24 ml-2 md:ml-4 text-center bg-white text-purple-500 
-        border-white border font-bold rounded-full shadow-[0_0_1rem_0px_#9d5ae3]
-        hover:bg-purple-500 hover:text-white focus:bg-purple-500 focus:text-white"
-    >
-        {name}
-    </button>
-    ));
-
-    const dropdownContent = (
-      <div className="dropdown-content">
-        {buttonNames.map((name, index) => (
-          <a key={index} href="#" className="block px-4 py-2 border rounded-sm text-white bg-purple-500 hover:bg-white hover:text-purple-500 active:bg-white active:text-purple-500">
-            {name}
-          </a>
-        ))}
-      </div>
-    );
-
-    const [isOpen, setIsOpen] = useState(false);
-
-    return (
-      <section className="flex flex-col md:flex-row">
-        <div className="w-full md:w-3/5 m-4 md:m-8">
-          <div className='flex'>
-            <div className='flex w-10/12 h-10 mb-8 mr-6 bg-purple-500 text-white
-            border-white border font-bold rounded-lg shadow-[0_0_1rem_0px_#9d5ae3]'>
-              <Image 
-                src="/search.png"
-                alt='searchImg'
-                width={25}
-                height={25}
-                className='m-2 ml-6'
-                />
-                <p className='m-2'>Search for..</p>
-                
-            </div>
-            <div className='w-24 h-10 mb-8 flex justify-center items-center bg-purple-500 text-white border-white border font-bold rounded-lg shadow-[0_0_1rem_0px_#9d5ae3]'>
-              <PiFunnel className='text-3xl '/>
-            </div>
+  return (
+    <section className="flex flex-col md:flex-row">
+      <div className="w-full md:w-3/5 m-4 md:m-8">
+        <div className="flex">
+          <div
+            className="flex w-10/12 items-center mb-8 mr-6 bg-purple-500 text-white
+            border-white border font-bold rounded-lg shadow-[0_0_1rem_0px_#9d5ae3]"
+          >
+            <Image
+              src="/search.png"
+              alt="searchImg"
+              width={25}
+              height={25}
+              className="m-2 ml-6"
+            />
+            <input
+              type="text"
+              className="m-2 border-none bg-transparent text-white placeholder:text-white outline-none w-full h-full"
+              placeholder="Search For..."
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+            />
           </div>
 
-        <div>   
-              
+          <div className="w-24 h-10 mb-8 flex justify-center items-center bg-purple-500 text-white border-white border font-bold rounded-lg shadow-[0_0_1rem_0px_#9d5ae3]">
+            <PiFunnel className="text-3xl " />
+          </div>
+        </div>
+        <div>
           <div className="relative inline-block text-left md:hidden">
             <div>
-              <button onClick={() => setIsOpen((prev) => !prev)} 
-                    className='flex items-center justify-center p-4 w-[150px] h-[40px] font-bold border-2 border-purple-500 text-white rounded-lg absolute bottom-0'>
-                      SORT BY
+              <button
+                onClick={() => setIsOpen(!prev)}
+                className="flex items-center justify-center p-4 w-[150px] h-[40px] font-bold border-2 border-purple-500 text-white rounded-lg absolute bottom-0"
+              >
+                SORT BY
                 {!isOpen ? (
-                  <RxCaretDown className='h-14 text-4xl' />
+                  <RxCaretDown className="h-14 text-4xl" />
                 ) : (
-                  <RxCaretUp className='h-14 text-4xl'/>
+                  <RxCaretUp className="h-14 text-4xl" />
                 )}
               </button>
-            {isOpen && (
-              <div className='absolute w-[150px]'>
-                {dropdownContent}
-              </div>
-            )}
+              {isOpen && (
+                <div className="absolute w-[150px]">
+                  {buttonNames.map((name, index) => (
+                    <a
+                      key={index}
+                      href="#"
+                      className="block px-4 py-2 border rounded-sm text-white bg-purple-500 hover:bg-white hover:text-purple-500 active:bg-white active:text-purple-500"
+                      onClick={() => {
+                        console.log(condition);
+                        setCondition(name);
+                      }}
+                      onBlur={() => setCondition(null)}
+                    >
+                      {name}
+                    </a>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
 
-          <div className="p-4 h-20 mb-4 border-solid border-purple-500 rounded-lg border-2 hidden md:flex items-center justify-center">
-           <div className='flex'>
-            <div className="font-bold text-white py-1">
-              SORT BY:
-              {buttonElements}
-            </div>
-           </div>
+          <div className="font-bold text-white py-1 p-4 h-20 mb-4 border-solid border-purple-500 rounded-lg border-2 hidden md:flex items-center justify-center">
+            SORT BY:
+            {buttonNames.map((name, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  console.log(condition);
+                  setCondition(name);
+                }}
+                onBlur={() => setCondition(null)}
+                className="w-22 h-10 md:w-24 ml-2 md:ml-4 text-center bg-white text-purple-500 border-white border font-bold rounded-full shadow-[0_0_1rem_0px_#9d5ae3]hover:bg-purple-500 hover:text-white focus:bg-purple-500 focus:text-white"
+              >
+                {name}
+              </button>
+            ))}
           </div>
-       
         </div>
-          
-          
-            <CommunityPosts />
-            <CommunityPosts />
-            <CommunityPosts />
-
-          
+        {postFilter(searchFilter(posts, search), condition).map(
+          (post, index) => (
+            <CommunityPosts key={index} post={post} />
+          )
+        )}
       </div>
-        
 
+      <div className="w-full md:w-5/12 m-4 md:m-8">
+        <div className="w-656 h-300 p-4 border-solid border-purple-500 rounded-lg border-2">
+          <p className="font-bold text-white">Welcome, tech enthusiast!</p>
 
-        <div className="w-full md:w-5/12 m-4 md:m-8">
-          <div class="w-656 h-300 p-4 border-solid border-purple-500 rounded-lg border-2">
-            <p className="font-bold text-white">Welcome, tech enthusiast!</p>
+          <p className="text-white">
+            Glad you&apos;ve found a way to our community page. It&apos;s a
+            vibrant space filled with passionate people just like you, all eager
+            to discuss, learn, and share about their latest tech trends,
+            gadgets, and ideas.
+          </p>
 
-            <p className="text-white">Glad you&apos;ve found a way to our community page. It&apos;s a vibrant space filled with passionate
-                people just like you, all eager to discuss, learn, and share about their latest tech trends, gadgets, and ideas.
-            </p>
+          <hr className="mt-4 border-t border-purple-500"></hr>
 
-            <hr class="mt-4 border-t border-purple-500"></hr>
-
-            <div className="mt-4 py-2 text-center bg-purple-500 text-white border-white border font-bold rounded-full shadow-[0_0_1rem_0px_#9d5ae3]">
+          <div className="mt-4 py-2 text-center bg-purple-500 text-white border-white border font-bold rounded-full shadow-[0_0_1rem_0px_#9d5ae3]">
             <Link href="/create-post">Create Post</Link>
-            </div>
-                    
-            <div
-              className="mt-4 py-2 text-center bg-white text-purple-500 border-white border font-bold rounded-full shadow-[0_0_1rem_0px_#9d5ae3]">
-                Create Community
-            </div>
           </div>
 
-          <div class="w-656 h-300 p-4 mt-8 border-solid border-purple-500 rounded-lg border-2">
-            <p className="text-white text-3xl font-bold">Recently Viewed Posts</p>
-
-            <p className="text-white font-bold pt-4">Heading of recent post</p>
-
-            <p className="text-white">Glad you&apos;ve found a way to our community page. It&apos;s a vibrant space filled with passionate
-              people just like you, all eager to discuss, learn, and share about their latest tech trends, gadgets, and ideas.
-            </p>
-
-            <div className='flex mt-4'>
-              <CgProfile className='mr-2 mt-1'/>
-              <p>XX views</p>
-              <VscTriangleUp className='ml-4 mr-2 text-2xl'/>
-              <p>XX upvotes</p>
-            </div>
-
+          <div className="mt-4 py-2 text-center bg-white text-purple-500 border-white border font-bold rounded-full shadow-[0_0_1rem_0px_#9d5ae3]">
+            Create Community
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default Community;
-  
+
+        <div className="w-656 h-300 p-4 mt-8 border-solid border-purple-500 rounded-lg border-2">
+          <p className="text-white text-3xl font-bold">Recently Viewed Posts</p>
+
+          <p className="text-white font-bold pt-4">Heading of recent post</p>
+
+          <p className="text-white">
+            Glad you&apos;ve found a way to our community page. It&apos;s a
+            vibrant space filled with passionate people just like you, all eager
+            to discuss, learn, and share about their latest tech trends,
+            gadgets, and ideas.
+          </p>
+
+          <div className="flex mt-4">
+            <CgProfile className="mr-2 mt-1" />
+            <p>XX views</p>
+            <VscTriangleUp className="ml-4 mr-2 text-2xl" />
+            <p>XX upvotes</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Community;
--- src/components/community/communityPosts.jsx
+++ src/components/community/communityPosts.jsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React from "react";
 import Image from "next/image";
 import CommunityContent from "./communityContent";
 import { VscTriangleUp } from "react-icons/vsc";
@@ -7,73 +7,55 @@ import { VscTriangleDown } from "react-icons/vsc";
 import { FaShare } from "react-icons/fa";
 import { TfiCommentAlt } from "react-icons/tfi";
 import CommunityComment from "./communityComment";
+import { likePost } from "@/actions/post";
 
-const CommunityPosts = () => {
-   
-    const posts = [
-      {
-        category: "Web Development",
-        user: "User [xxxx]",
-        date: "DD/MM/YY",
-        title: "Some Article about WebDev",
-      },
-    ];
-  
-    const postElements = posts.map((post, index) => (
-        <div key={index}>
-            <div key={index} className=" border-solid border-purple-500 rounded-lg border-2 p-4 text-white">
-              <div className='flex'>
-                <div className="mr-6 mt-1 h-10 w-10 border-2 border-slate-500 bg-slate-500 rounded-full">
-                  <Image alt='user_img'/>
-                </div>
-                <div>
-                  <p className="font-bold">From: {post.category}</p>
-                  <p>User {post.user} Posted on {post.date}</p>
-                </div>
-              </div>
-              <p className="font-bold text-3xl my-4">{post.title}</p>
-              <div>
-                <CommunityContent />
-              </div>
-              <div className='flex justify-between'>
-                <div className='flex'>
-                  <VscTriangleDown className='mr-2 text-5xl' />  
-                  <p className='pt-3'>XX.X</p>
-                </div>
-
-                <div className='flex'>
-                  <VscTriangleUp className='mr-2 text-5xl'/>  
-                  <p className='pt-3'>XX.X</p>
-                </div>
-
-                <div className='flex pt-3'>
-                  <FaShare className='mr-2 text-3xl'/>  
-                  <p>XX.X</p>
-                </div>
-
-                <div className='flex pt-3'>
-                  <TfiCommentAlt className='mr-2 text-3xl'/>  
-                  <p>XX.X</p>
-                </div>
-              </div>
-            </div>
-
-            <div className='flex justify-end'>
-                <CommunityComment />
-            </div>
+const CommunityPosts = ({ post }) => {
+  return (
+    <section className="my-4">
+      <div className=" border-solid border-purple-500 rounded-lg border-2 p-6 text-white">
+        <div className="flex">
+          <div className="mr-6 mt-1 h-10 w-10 border-2 border-slate-500 bg-slate-500 rounded-full">
+            {/* <Image alt="user_img" /> */}
+          </div>
+          <div>
+            <p className="font-bold">From: {post.domain}</p>
+            <p>
+              User {post.user.name} Posted on{" "}
+              {new Date(post.createdAt).toLocaleDateString()}
+            </p>
+          </div>
         </div>
-      
+        <p className="font-bold text-3xl my-4">{post.title}</p>
+        <CommunityContent content={post.content} />
+        <div className="flex justify-between">
+          <button onClick={() => likePost(post.id, 0)} className="flex">
+            <VscTriangleDown className="mr-2 text-5xl" />
+            <p className="pt-3">
+              {post.postLikes.filter((like) => like.type === "DISLIKE").length}
+            </p>
+          </button>
+          <button onClick={() => likePost(post.id, 1)} className="flex">
+            <VscTriangleUp className="mr-2 text-5xl" />
+            <p className="pt-3">
+              {post.postLikes.filter((like) => like.type === "LIKE").length}
+            </p>
+          </button>
+          <div className="flex pt-3">
+            <FaShare className="mr-2 text-3xl" />
+            <p>{post.savedPosts.length}</p>
+          </div>
 
-    ));
-  
-    return (
-      <section className="flex">
-        <div className="my-4">
-          {postElements}
+          {/* <div className="flex pt-3">
+            <TfiCommentAlt className="mr-2 text-3xl" />
+            <p>XX.X</p>
+          </div> */}
         </div>
-      </section>
-    );
-  };
-  
-  export default CommunityPosts;
-  
+      </div>
+      {/* <div className="text-right">
+        <CommunityComment />
+      </div> */}
+    </section>
+  );
+};
+
+export default CommunityPosts;
