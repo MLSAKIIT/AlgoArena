442eb3c547e7aefa170a0b307c4a623c07290234
VinamraSaurav <80186642+VinamraSaurav@users.noreply.github.com>
2024-03-08 22:09:07 +0530

    updated saved courses page

--- src/app/dashboard/saved-courses/page.jsx
+++ src/app/dashboard/saved-courses/page.jsx
@@ -1,12 +1,14 @@
 import Dashboard from "@/components/dashboard/Dashboard";
 import Navbar from "@/components/ui/Navbar";
 import StaredCourse from "@/components/startedCourses/StaredCourse";
+import DashboardSavedCourses from "@/components/startedCourses/DashboardSavedCourses";
 
 export default function dashboard() {
   return (
     <>
       <Navbar />
-      <StaredCourse />
+      {/* <StaredCourse /> */}
+      <DashboardSavedCourses username="AYUSH RANJAN"/>
       {/* <Dashboard username="AYUSH RANJAN"></Dashboard> */}
     </>
   );
--- src/components/startedCourses/StaredCourse.jsx
+++ src/components/startedCourses/StaredCourse.jsx
@@ -5,13 +5,13 @@ import StaredCourseCard from "./StaredCourseCard";
 const StaredCourse = () => {
   return (
     <div className="max-w-7xl mt-4 mx-auto mb-10">
-      <div className="font-popins text-4xl font-[900] bg-gradient-course bg-clip-text text-transparent m-3 hidden md:block w-[400px]">
+      <div className="font-popins text-4xl font-[900] bg-gradient-course bg-clip-text text-transparent  hidden md:block w-[400px]">
         Your Stared Courses
       </div>
-      <div className="font-popins text-4xl font-[900] bg-gradient-course bg-clip-text text-transparent m-3 block md:hidden">
+      <div className="font-popins text-4xl font-[900] bg-gradient-course bg-clip-text text-transparent block md:hidden">
         Stared Courses
       </div>
-      <div className="my-9 m-3">
+      <div className="my-9">
         {data.map((course, i) => (
           <StaredCourseCard key={i} course={course} />
         ))}
