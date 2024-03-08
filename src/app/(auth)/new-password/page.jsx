b87ec66c174034308d58905f92af536de4ab000e
Rupoum <rupoummitra15278@gmail.com>
2024-03-09 03:07:38 +0530

    improved all the forms and also made the verification page

--- src/app/(auth)/new-password/page.jsx
+++ src/app/(auth)/new-password/page.jsx
@@ -1,16 +1,17 @@
-import NewPasswordForm from '@/components/auth/new-password/new-password-form'
-import { notFound } from 'next/navigation'
-import React from 'react'
+import NewPasswordForm from "@/components/auth/new-password/new-password-form";
+import { notFound } from "next/navigation";
+import React from "react";
 
 const NewPasswordPage = ({ searchParams }) => {
-  if (!searchParams.token){
-    notFound()
+  if (!searchParams.token) {
+    notFound();
   }
   return (
-    <div className='container relative flex pt-20 flex-col items-center justify-center lg:px-0'>
-        <NewPasswordForm token={searchParams.token} />
+    <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+      {/* <NewPasswordForm token={searchParams.token} /> */}
+      <NewPasswordForm />
     </div>
-  )
-}
+  );
+};
 
-export default NewPasswordPage
+export default NewPasswordPage;
--- src/components/auth/new-password/new-password-form.jsx
+++ src/components/auth/new-password/new-password-form.jsx
@@ -4,6 +4,7 @@ import { newPasswordSchema } from "@/schemas/auth/new-password";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { useFormik } from "formik";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -20,7 +21,7 @@ import { toast } from "sonner";
 import { Loader2 } from "lucide-react";
 import { newPassword } from "@/actions/new-password";
 
-const NewPasswordForm = ( { token }) => {
+const NewPasswordForm = ({ token }) => {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
 
@@ -36,44 +37,70 @@ const NewPasswordForm = ( { token }) => {
             toast.error(data.error);
           } else {
             toast.success(data.success);
-            router.push("/sign-in")
+            router.push("/sign-in");
           }
         });
       });
     },
   });
   return (
-    <form onSubmit={handleSubmit} className="space-y-8">
-      <Card>
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Reset Your Password</CardTitle>
-          <CardDescription>Enter your new password</CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              name="password"
-              placeholder="********"
-              type="password"
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={cn(errors.password && "border-red-500")}
-            />
-            {errors.password && (
-              <p className="text-red-500 text-sm">{errors.password}</p>
-            )}
-          </div>
-        </CardContent>
-        <CardFooter className="flex flex-col items-start gap-5">
-          <Button className="w-full" type="submit" disabled={isPending}>
-            Reset Password
-            {isPending && <Loader2 className="animate-spin h-4 w-4 ml-2" />}
-          </Button>
-        </CardFooter>
-      </Card>
-    </form>
+    <div className=" relative h-screen overflow-hidden flex py-10 flex-col items-center justify-center flex-0 flex-auto lg:px-0">
+      <form onSubmit={handleSubmit} className="space-y-8">
+        <Card className="sm:bg-color-6 w-full sm:w-96 sm:rounded-2xl rounded-none sm:border-solid border-none">
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl flex justify-center">
+              Reset Your Password
+            </CardTitle>
+            <CardDescription>Enter your new password</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                name="password"
+                placeholder="********"
+                type="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={cn(errors.password && "border-red-500")}
+              />
+              {errors.password && (
+                <p className="text-red-500 text-sm">{errors.password}</p>
+              )}
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col items-start gap-5">
+            <Button className="w-full" type="submit" disabled={isPending}>
+              Reset Password
+              {isPending && <Loader2 className="animate-spin h-4 w-4 ml-2" />}
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+      <Image
+        src="/assets/icons/Ellipse3.svg"
+        alt="Ellipse"
+        height={100}
+        width={24}
+        className="fixed min-[1536px]:hidden -z-10 w-[80rem] h-[75rem] left-[-20rem] top-[5rem] opacity-60 lg:block hidden "
+      />
+      <Image
+        src="/assets/icons/Ellipse3.svg"
+        alt="Ellipse"
+        height={100}
+        width={24}
+        className="fixed min-[1536px]:hidden -z-10 w-[68.0625rem] h-[56.5rem] left-[-16rem] top-[16rem] opacity-50 lg:block hidden"
+      />
+      <Image
+        src="/assets/icons/Ellipse3.svg"
+        alt="Ellipse"
+        height={100}
+        width={24}
+        className="fixed min-[1536px]:hidden -z-10 w-[51.5rem] h-[44.5rem] left-[-10rem] top-[24rem] overflow-hidden opacity-50 lg:block hidden"
+      />
+      <div className="fixed min-[1536px]:hidden -z-10 w-[15rem] h-[15rem] left-[5rem] bg-color-2 rounded-full blur-2xl bottom-[-10rem] opacity-50 lg:block hidden" />
+    </div>
   );
 };
 
