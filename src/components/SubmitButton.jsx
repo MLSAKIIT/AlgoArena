7349cec0137b8d0b5765175abf0ab77bd3153700
BT08249 <adarshgku@gmail.com>
2024-03-08 20:36:19 +0530

    merged incoming

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -9,11 +9,7 @@ const SubmitButton = ({ children, ...props }) => {
   const { pending } = useFormStatus();
   return (
     <Button disabled={pending} {...props}>
-<<<<<<< HEAD
-      {pending ? <Loader2 className="h-4 w-4" /> : children}
-=======
       {pending ? <Loader2 className="h-4 w-4 animate-spin" /> : children}
->>>>>>> 27cfa88623dc45a3e0eb5f6b54c1f669628ee95f
     </Button>
   );
 };
