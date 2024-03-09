7d95263fad3ef54e0677dd95b27bef2a74c658da
Akangkha Sarkar <akangkhasarkar@gmail.com>
2024-03-09 14:19:32 +0530

    "Created the domain page for learning paths"

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Community from "@/components/community/communityPage";
-
+import Navbar from '@/components/ui/Navbar';
 export const page = () => {
   return (
     <div>
+      <Navbar/>
         <Community />
     </div>
   )
