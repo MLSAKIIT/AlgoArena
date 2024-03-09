505f35d2db47b58000b34aafc64ac016825d2ea7
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-03-09 14:46:47 +0530

    Changed not-found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,14 +1,30 @@
-import React from "react";
+"use client";
+
+import React, {useState, useEffect} from "react";
 import Image from "next/image";
-import Navbar from "@/components/ui/Navbar";
 import Link from "next/link";
 
 export default function NotFound() {
+    const [countdown, setCountdown] = useState(9);
+
+    // useEffect(() => {
+    //     const timer = setInterval(() => {
+    //         if (countdown >= 1) {
+    //             setCountdown(countdown - 1);
+    //         } else {
+    //             clearInterval(timer);
+    //             window.location.href = "/";
+    //         }
+    //     }, 1000);
+
+    //     return () => clearInterval(timer);
+    // }, [countdown]);
+
     return (
         <>
-            <div className="relative h-screen flex py-10 flex-col items-center justify-center flex-0 flex-auto lg:px-0">
-                <div className="w-full flex justify-center items-center">
-                    <div className="flex flex-col items-center justify-center">
+            <div className="h-screen flex py-10 flex-col items-center justify-center flex-0 flex-auto lg:px-0">
+                <div className="w-full flex justify-center items-center  border-2 border-color-2 sm:w-1/5 sm:h-4/6 mx-auto my-auto">
+                    <div className=" box-border mt-8 grid place-items-center py-1 px-1 bg-[#9d5ae3] text-white w-40 h-14 rounded-full shadow-[0_0_1rem_0px_#9d5ae3] text-xl  outline-none border-none [text-shadow:_0px_0px_5px_rgb(0_0_0)] cursor-pointer hover:bg-[#7f3dbf] active:scale-95 transition duration-200">
                         <Image src="/Error.svg" className="sm:w-100 sm:h-100 lg:w-200 lg:h-200" width={200} height={200} alt="Error Image" />
                         <h1 className="mt-2 text-center text-2xl md:text-3xl text-wrap">The resource you were looking for was not found..</h1>
                     </div>
@@ -37,27 +53,8 @@ export default function NotFound() {
                     className="fixed min-[1536px]:hidden -z-10 w-[51.5rem] h-[44.5rem] left-[-10rem] top-[24rem] overflow-hidden opacity-50 lg:block hidden"
                 />
                 <div className="fixed min-[1536px]:hidden -z-10 w-[15rem] h-[15rem] left-[5rem] bg-color-2 rounded-full blur-2xl bottom-[-10rem] opacity-50 lg:block hidden" />
-                <Link href="/" className=" flex justify-center items-center box-border mt-8 py-1 bg-[#9d5ae3] text-white w-40 h-14 rounded-full shadow-[0_0_1rem_0px_#9d5ae3] text-xl outline-none border-none [text-shadow:_0px_0px_5px_rgb(0_0_0)] cursor-pointer hover:bg-[#7f3dbf] active:scale-95 transition duration-200 z-1">
-                    Go to Home
-                </Link>
+                <div className="mt-8 text-center text-xl text-white">Redirecting in {countdown}..</div>
             </div>
         </>
     );
 }
-
-// /* AlgoArena */
-
-// position: absolute;
-// width: 142px;
-// height: 25px;
-// left: calc(50% - 142px/2);
-// top: 21px;
-
-// font-family: 'The Bold Font';
-// font-style: normal;
-// font-weight: 700;
-// font-size: 25px;
-// line-height: 25px;
-// /* identical to box height */
-
-// color: #FFFFFF;
