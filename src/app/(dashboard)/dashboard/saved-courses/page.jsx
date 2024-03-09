45c6428fd2c7a3b9cdd1ed8347abb55386151958
Vaibhav <68665948+dead8309@users.noreply.github.com>
2024-03-09 16:18:06 +0530

    fix: pages rendering as static during build time

--- src/app/(dashboard)/dashboard/saved-courses/page.jsx
+++ src/app/(dashboard)/dashboard/saved-courses/page.jsx
@@ -1,12 +1,10 @@
-import Navbar from "@/components/ui/Navbar";
-import StaredCourse from "@/components/startedCourses/StaredCourse";
 import DashboardStaredCourses from "@/components/startedCourses/DashboardStaredCourses";
-import { getAllLearningPathsWithProgress, getSavedLearningPathsWithProgress } from "@/data/dashboard";
+import { getSavedLearningPathsWithProgress } from "@/data/dashboard";
 import Sidebar from "@/components/sidebar/Sidebar";
 
+export const dynamic = "force-dynamic"
+
 export default async function dashboard() {
-  const data = await getAllLearningPathsWithProgress()
-  const currentlyWatchingLearningPaths = data.filter((learningPath) => learningPath.progress < 100);
   const savedLearningPaths = await getSavedLearningPathsWithProgress();
   return (
     <div className="flex">
--- src/app/(dashboard)/dashboard/your-courses/page.jsx
+++ src/app/(dashboard)/dashboard/your-courses/page.jsx
@@ -1,12 +1,12 @@
 import Dashboard from "@/components/dashboard/Dashboard";
 import Sidebar from "@/components/sidebar/Sidebar";
-import Navbar from "@/components/ui/Navbar";
 import { getAllLearningPathsWithProgress } from "@/data/dashboard";
+
+export const dynamic = "force-dynamic"
 
 export default async function dashboard() {
   const data = await getAllLearningPathsWithProgress()
   const currentlyWatchingLearningPaths = data.filter((learningPath) => learningPath.progress < 100);
-  const completedLearningPaths = data.filter((learningPath) => learningPath.progress === 100);
   return (
     <div className="flex ">
       <Sidebar name="Ayush Ranjan"></Sidebar>
