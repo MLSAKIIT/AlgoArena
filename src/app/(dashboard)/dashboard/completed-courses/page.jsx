9968c340f759420a5d74fd29bfbb202f2d3f5ca3
Utkarsh <utkarshjha.4009@gmail.com>
2024-03-09 19:15:24 +0530

    Completed courses connected with db

--- src/app/(dashboard)/dashboard/completed-courses/page.jsx
+++ src/app/(dashboard)/dashboard/completed-courses/page.jsx
@@ -1,12 +1,15 @@
+
 import CompletedCourses from "@/components/dashboard/CompletedCourses";
 import Sidebar from "@/components/sidebar/Sidebar";
+import { getCompletedLearningPaths } from "@/data/dashboard";
 
-export default function dashboard() {
+export default async function dashboard() {
+  const data = await getCompletedLearningPaths()
   return (
     <div className="flex w-screen">
-      <Sidebar></Sidebar>
+      <Sidebar/>
       <div className="md:absolute xl:w-[80%] lg:w-[70%]  md:w-[96%] lg:left-[20%] 2xl:left-[14%] mt-20 sm:mt-12">
-      <CompletedCourses username="AYUSH RANJAN"></CompletedCourses>
+      <CompletedCourses data={data}></CompletedCourses>
       </div>
     </div>
   );
