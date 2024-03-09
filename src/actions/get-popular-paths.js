08d039b4ba9e445e0422a8ae14ec640d968916e3
BT08249 <adarshgku@gmail.com>
2024-03-09 16:33:53 +0530

    fear/get popular path action

---
+++ src/actions/get-popular-paths.js
@@ -0,0 +1,24 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+const { db } = require("@/server/db");
+
+export const getPopularPaths = async () => {
+  try {
+    const learningPaths = await db.learningPath.findMany({
+      orderBy: {
+        savedLearningPaths: {
+          _count: "desc",
+        },
+      },
+      take: 10,
+      include: {
+        savedLearningPaths: true,
+      },
+    });
+    return learningPaths;
+  } catch (error) {
+    console.error("Error fetching learning paths:", error);
+    return { error: "Failed to fetch learning paths" };
+  }
+};
