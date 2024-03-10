67c3473ded83a81830bbf8f198d25ec5df7d8a6c
Vaibhav <68665948+dead8309@users.noreply.github.com>
2024-03-10 09:53:24 +0530

    feat: configure metadata

--- src/constants/siteConfig.js
+++ src/constants/siteConfig.js
@@ -1,15 +1,63 @@
 export const siteConfig = {
-    navBarLinks: {
-        home: "/",
-        learningPath: "/learning-paths",
-        community: "/community",
-        aboutUs: "/aboutUs",
-        dashboard: "/dashboard",
-    },
-    socials: {
-        github: 'https://github.com/MLSAKIIT',
-        discord: 'https://discord.gg/BW28wXGfsZ',
-        instagram: 'https://www.instagram.com/mlsakiit/',
-        facebook: 'https://www.facebook.com/msakiit'
-    }
-}
+  title: "Algoarena",
+  description:
+    "This MLSA platform aims to offer personalized learning journeys, diverse educational content, user accounts for tracking progress and user-generated content. It incorporates gamification elements for engagement and provides a free Udemy-like experience, including course tracking, reviews, and certificates of completion.",
+  navBarLinks: {
+    home: "/",
+    learningPath: "/learning-paths",
+    community: "/community",
+    aboutUs: "/aboutUs",
+    dashboard: "/dashboard",
+  },
+  socials: {
+    github: "https://github.com/MLSAKIIT",
+    discord: "https://discord.gg/BW28wXGfsZ",
+    instagram: "https://www.instagram.com/mlsakiit/",
+    facebook: "https://www.facebook.com/msakiit",
+  },
+  icons: {
+    shortcut:  ["/favicon.ico"],
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/icons/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/icons/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/icons/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+      {
+        url: "/icons/favicon-196x196.png",
+        type: "image/png",
+        sizes: "196x196",
+      },
+    ],
+    apple: [
+      { url: "/icons/apple-touch-icon-57x57.png", sizes: "57x57", type: "image/png" },
+      { url: "/icons/apple-touch-icon-60x60.png", sizes: "60x60", type: "image/png" },
+      { url: "/icons/apple-touch-icon-72x72.png", sizes: "72x72", type: "image/png" },
+      { url: "/icons/apple-touch-icon-76x76.png", sizes: "76x76", type: "image/png" },
+      {
+        url: "/icons/apple-touch-icon-114x114.png",
+        sizes: "114x114",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-120x120.png",
+        sizes: "120x120",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-152x152.png",
+        sizes: "152x152",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+};
